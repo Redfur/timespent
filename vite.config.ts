92bb97d35a485b37cf50d5c86580cee8a29e0763
g.ivanov <murzikcheb@gmail.com>
2025-07-10 23:17:11 +0300

    add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import path from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 	base: process.env.NODE_ENV === 'production' ? '/timespent/' : '/',
 	resolve: {
 		alias: {
-			'~': path.resolve(__dirname, './src/'),
+			'@': path.resolve(__dirname, './src/'),
 		},
 	},
 });
